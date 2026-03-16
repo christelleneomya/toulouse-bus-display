ac06f30c1b176a84f986e138177c415839648016
Christelle <christelle@neomya.io>
2026-03-16 18:16:24 +0100

    Ajout fonction Netlify Tisseo

--- netlify/functions/netlify/functions/tisseo.js
+++ netlify/functions/netlify/functions/tisseo.js
@@ -1,36 +1,35 @@
 exports.handler = async function(event) {
-
-  const params = new URLSearchParams(event.queryStringParameters)
-
-  const url =
-    "https://api.tisseo.fr/v2/stops_schedules.json?" +
-    params.toString()
-
   try {
+    const params = new URLSearchParams(event.queryStringParameters || {});
+    const url = "https://api.tisseo.fr/v2/stops_schedules.json?" + params.toString();
 
-    const response = await fetch(url)
+    const response = await fetch(url, {
+      headers: {
+        "Accept": "application/json"
+      }
+    });
 
-    const data = await response.text()
+    const body = await response.text();
 
     return {
       statusCode: response.status,
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json; charset=utf-8",
         "Access-Control-Allow-Origin": "*"
       },
-      body: data
-    }
-
+      body
+    };
   } catch (error) {
-
     return {
       statusCode: 500,
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+        "Access-Control-Allow-Origin": "*"
+      },
       body: JSON.stringify({
         error: "Proxy error",
         details: error.message
       })
-    }
-
+    };
   }
-
-}
+};
